1d49fe2f35c9d7eac71d3fa906cd97de37137360
CiroosAI <ciroosai@gmail.com>
2025-11-24 06:29:10 +0700

    Fix: Rename responseData variable to avoid conflict with service response

--- controllers/payment/qris_payment_controller.go
+++ controllers/payment/qris_payment_controller.go
@@ -297,7 +297,7 @@ func (qpc *QRISPaymentController) CreateQRIS(c *gin.Context) {
 	requestTime := now.Format(time.RFC3339)
 
 	// Build response with consistent field order
-	responseData := gin.H{
+	responseDataMap := gin.H{
 		"id":           grantID,
 		"reference_id": reqBody.ReferenceID,
 		"amount":       reqBody.Amount,
@@ -317,7 +317,7 @@ func (qpc *QRISPaymentController) CreateQRIS(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"response_code":    "2000500",
 		"response_message": "Successful",
-		"response_data":   responseData,
+		"response_data":   responseDataMap,
 	})
 }
 
--- controllers/payment/va_payment_controller.go
+++ controllers/payment/va_payment_controller.go
@@ -345,7 +345,7 @@ func (vac *VAPaymentController) CreateVA(c *gin.Context) {
 	requestTime := now.Format(time.RFC3339)
 
 	// Build response with consistent field order
-	responseData := gin.H{
+	responseDataMap := gin.H{
 		"id":           grantID,
 		"reference_id": reqBody.ReferenceID,
 		"amount":       reqBody.Amount,
@@ -367,7 +367,7 @@ func (vac *VAPaymentController) CreateVA(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"response_code":    "2000400",
 		"response_message": "Successful",
-		"response_data":   responseData,
+		"response_data":   responseDataMap,
 	})
 }
 
